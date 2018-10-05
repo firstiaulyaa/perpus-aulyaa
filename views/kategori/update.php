a5517ae0c8ef71164a13e7229a696d494c38ce62
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:18:43 +0700

    Tampilan update pada Kategori

--- views/kategori/update.php
+++ views/kategori/update.php
@@ -5,17 +5,17 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Kategori */
 
-$this->title = 'Update Kategori: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Kategoris', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->title = 'Ubah Kategori Buku : ' . $model->nama; // memanggil nama pada data kategori
+$this->params['breadcrumbs'][] = ['label' => 'Kategori', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->nama, 'url' => ['view', 'id' => $model->nama]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="kategori-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="box box-primary">
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
 </div>
