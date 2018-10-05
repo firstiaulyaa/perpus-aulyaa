b65ee34b3c1f14032e4b0bf64679b288c4bff1a4
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 13:39:11 +0700

    Model Kategori

--- models/Kategori.php
+++ models/Kategori.php
@@ -41,4 +41,81 @@ class Kategori extends \yii\db\ActiveRecord
             'nama' => 'Nama',
         ];
     }
+
+    /**
+     * @inheritdoc
+     * @return array untuk dropdown
+     */
+
+
+
+    // -------------------------------------------------------------- //
+    // untuk menampilkan (list) data kategori berdasarkan id dan nama //
+    // -------------------------------------------------------------- //
+
+    public static function getList()
+        {
+            return \yii\helpers\ArrayHelper::map(self::find()->all(), 'id', 'nama');
+        }
+
+    // -------------------------------------------------------------- //
+
+
+    
+    // ---------------------------------------------------------------------------- //
+    // untuk mencari dan memanggil semua data buku berdasarkan id_kategori dan nama //
+    // ---------------------------------------------------------------------------- //
+
+    public function findAllBuku()
+        {
+            return Buku::find()
+            ->andWhere(['id_kategori' => $this->id])
+            ->orderBy(['nama' => SORT_ASC])
+            ->all();
+        }
+
+    // ------------------------------------------------------------- //
+
+    
+
+    // --------------------------------------- //
+    // untuk melakukan hitung data pada grafik //
+    // --------------------------------------- //
+
+    public static function getCount()
+        {
+            return static::find()->count();
+        }
+
+    // --------------------------------------- //
+
+
+
+    // ----------------------------------------------------------------- //
+    // Mengambil semua data yang ada di tabel buku yang dimana id buku akan ditampilkan berdasarkan id_*** / id_*** akan mengambil data di buku yang berkaitan dengan id_*** //
+    // ----------------------------------------------------------------- //
+
+    public function getManyBuku()
+        {
+            return $this->hasMany(Buku::class, ['id_kategori' => 'id']);
+        }
+
+    // ------------------------------------------------------------- //
+
+
+
+    // ------------------------------------------------------ //
+    // Menjumlah semua data buku yang berkaitan dengan id_*** //
+    // ------------------------------------------------------ //
+
+    public static function getGrafikList()
+        {
+            $data = [];
+            foreach (static::find()->all() as $kategori) {
+                $data[] = [$kategori->nama, (int) $kategori->getManyBuku()->count()];
+            }
+            return $data;
+        }
+
+    // ------------------------------------------------------ //
 }
