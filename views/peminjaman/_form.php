f714272b2643440f98dd850640a0f1b34eb228de
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:20:19 +0700

    Tampilan form pada Peminjaman

--- views/peminjaman/_form.php
+++ views/peminjaman/_form.php
@@ -2,6 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\date\DatePicker;
+use app\models\Buku;
+use yii\helpers\ArrayHelper;
+use app\models\Anggota; 
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Peminjaman */
@@ -12,16 +17,44 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id_buku')->textInput() ?>
+    <?= $form->field($model, 'id_buku')->widget(Select2::classname(), [
+            'data' =>  Buku::getList(),
+            'options' => [
+              'placeholder' => '- Pilih Buku -',
+            ],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+        ]); ?>
 
-    <?= $form->field($model, 'id_anggota')->textInput() ?>
+    <?= $form->field($model, 'id_anggota')->widget(Select2::classname(), [
+            'data' =>  Anggota::getList(),
+            'options' => [
+              'placeholder' => '- Pilih Anggota -',
+            ],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+    ]); ?>
 
-    <?= $form->field($model, 'tanggal_pinjam')->textInput() ?>
+    <?= $form->field($model, 'tanggal_pinjam')->widget(
+    DatePicker::className(), [
+        'pluginOptions' => [
+            'autoclose' => true,
+            'format' => 'dd-M-yyyy'
+        ]
+	]);?>
 
-    <?= $form->field($model, 'tanggal_kembali')->textInput() ?>
+	<?= $form->field($model, 'tanggal_kembali')->widget(
+    DatePicker::className(), [
+        'pluginOptions' => [
+            'autoclose' => true,
+            'format' => 'dd-M-yyyy'
+        ]
+    ]);?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Simpan', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
