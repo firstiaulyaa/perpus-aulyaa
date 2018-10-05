79c86246d09b717d167e284c514c8ca24d1a2eae
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 13:31:32 +0700

    Model User (update)

--- models/User.php
+++ models/User.php
@@ -17,6 +17,22 @@ use Yii;
 */
 class user extends \yii\db\ActiveRecord  implements \yii\web\IdentityInterface
 {
+    public static function getList()
+   {
+       return \yii\helpers\ArrayHelper::map(self::find()->all(), 'id', 'nama');
+   }
+   
+        //untuk menampilkan di peminjaman buku sebagai nama
+   public function getAnggota()
+   {
+       return $this->hasOne(Anggota::className(), ['id' => 'id_anggota']);
+   }
+
+        //untuk menampilkan di peminjaman buku sebagai nama
+   public function getPetugas()
+   {
+       return $this->hasOne(Petugas::className(), ['id' => 'id_petugas']);
+   }
    /**
     * {@inheritdoc}
     */
@@ -53,3 +69,81 @@ class user extends \yii\db\ActiveRecord  implements \yii\web\IdentityInterface
            'status' => 'Status',
        ];
    }
+
+    public static function findIdentity($id)
+   {
+       return self::findOne($id);
+   }
+
+   public static function findIdentityByAccessToken($token, $Type = null)
+   {
+       return static::findOne(['access_token' => $token]);
+   }
+   public function getId()
+   {
+       return $this->id;
+   }
+   public function getAuthKey()
+   {
+       return null;
+   }
+   public function validateAuthKey($authKey)
+   {
+       return $this->authKey === $authKey;
+   }
+
+   public static function findByUsername($username)
+   {
+       return self::findOne(['username'=>$username]);
+   }
+
+   public function validatePassword($password)
+   {
+       return $this->password == $password;
+   }
+  
+  //untuk validasi (session) untuk admin
+   public static function isAdmin()
+   {
+    if (Yii::$app->user->isGuest) {
+        return false;
+    }
+    $model = User::findOne(['username' => Yii::$app->user->identity->username]);
+    if ($model == null) {
+        return false;
+    } elseif ($model->id_user_role == 1) {
+        return true;
+    }
+    return false;
+   }
+
+   //untuk validasi anggota
+   public static function isAnggota()
+   {
+    if (Yii::$app->user->isGuest) {
+        return false;
+    }
+    $model = User::findOne(['username' => Yii::$app->user->identity->username]);
+    if ($model == null) {
+        return false;
+    } elseif ($model->id_user_role == 2) {
+        return true;
+    }
+    return false;
+   }
+
+   //untuk validasi petugas
+   public static function isPetugas()
+   {
+    if (Yii::$app->user->isGuest) {
+        return false;
+    }
+    $model = User::findOne(['username' => Yii::$app->user->identity->username]);
+    if ($model == null) {
+        return false;
+    } elseif ($model->id_user_role == 3) {
+        return true;
+    }
+    return false;
+   }
+}
