7a9f5f7b4d4040cb054d396c8f985072c22ed15d
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:36:22 +0700

    Tampilan create pada Petugas

--- views/petugas/create.php
+++ views/petugas/create.php
@@ -6,16 +6,17 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Petugas */
 
-$this->title = 'Create Petugas';
+$this->title = 'Tambah Data Petugas';
 $this->params['breadcrumbs'][] = ['label' => 'Petugas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="petugas-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+<div class="box box-primary">
+<div class="box-body">
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
+</div>
 </div>
