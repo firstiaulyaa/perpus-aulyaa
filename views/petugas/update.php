0b57092f7cf954f316bdde5257055f5afee8b173
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:37:59 +0700

    Tampilan update pada Petugas

--- views/petugas/update.php
+++ views/petugas/update.php
@@ -5,17 +5,19 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Petugas */
 
-$this->title = 'Update Petugas: ' . $model->id;
+$this->title = 'Ubah Data Petugas: ' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Petugas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="petugas-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="box box-primary">
+<div class="box-body">
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
+</div>
 </div>
