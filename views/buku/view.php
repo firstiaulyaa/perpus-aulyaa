5d1c7bfdcbf4e5ce9b3ba5bbba348444acf52b05
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:06:14 +0700

    Tampilan view pada BUku

--- views/buku/view.php
+++ views/buku/view.php
@@ -2,42 +2,93 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use app\models\Kategori;
+use app\models\Penulis;
+use app\models\Penerbit;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Buku */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Bukus', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+// kalau mau ada teks (misal: Buku)sebelum pemanggilan nama
+// $this->title = "Buku : " . $model->nama;
+
+$this->title = $model->nama; // memanggil nama dari data buku
+$this->params['breadcrumbs'][] = ['label' => 'Buku', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title; // memanggil judul dari halaman buku
 ?>
+
+<!-- menampilkan view buku -->
 <div class="buku-view">
+<div class="box box-primary">
+<div class="box-body">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
+<!-- button ubah dan hapus satu data buku -->
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Ubah', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Hapus', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Apakah Anda yakin ingin menghapusnya?',
                 'method' => 'post',
             ],
         ]) ?>
     </p>
+<!-- button ubah dan hapus satu data buku -->
 
-    <?= DetailView::widget([
+<!-- menampilkan satu data buku -->
+     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
-            'nama',
-            'tahun_terbit',
-            'id_penulis',
-            'id_penerbit',
-            'id_kategori',
+            // 'id',
+            [
+                'label' => 'Nama (Tahun)',
+                // 'attribute' => 'nama',
+                'value' => $model->nama . ' (' . $model->tahun_terbit . ')'
+            ],
+            [
+                'attribute' => 'tahun_terbit',
+                // Masehi -> untuk penambahan kata masehi dibelakang tahun
+                'value' => $model->tahun_terbit //. ' Masehi'
+            ],
+            [
+               'label' => 'Penulis',
+               'value' => function($data)
+                {
+                  return $data->getPenulis();
+                }
+           ],
+            [
+               'label' => 'Penerbit',
+               'value' => function($data)
+                {
+                  return $data->getPenerbit();
+                }
+           ],
+            [
+               'label' => 'Kategori',
+               'value' => function($data)
+                {
+                  return $data->getKategori();
+                }
+           ],
             'sinopsis:ntext',
-            'sampul',
+            [
+                'label' => 'Sampul',
+                'format' => 'raw',
+                'value' => function ($model) {
+                  if ($model->sampul != '') {
+                    return Html::img('@web/upload/sampul/' . $model->sampul, ['class' => 'img-responsive', 'style' => 'height:300px ']);
+                  } else {
+                    return '<div align="center"><h1>No Image</h1></div>';
+                  }
+                },
+            ],
             'berkas',
         ],
     ]) ?>
-
 </div>
+</div>
+</div>
+<!-- akhir menampilkan satu data buku -->
+<!-- akhir menampilkan view buku -->
