e8787cde8adcfa0a671bf1ddf8de78d7cccd593f
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:23:00 +0700

    Tampilan view pada Peminjaman

--- views/peminjaman/view.php
+++ views/peminjaman/view.php
@@ -2,38 +2,54 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use app\models\Buku;
+use app\models\Anggota;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Peminjaman */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Peminjamen', 'url' => ['index']];
+$this->title = $model->anggota->nama;
+$this->params['breadcrumbs'][] = ['label' => 'Peminjaman', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="peminjaman-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'id_buku',
-            'id_anggota',
-            'tanggal_pinjam',
-            'tanggal_kembali',
-        ],
+  <p>
+    <?= Html::a('Ubah', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+    <?= Html::a('Hapus', ['delete', 'id' => $model->id], [
+      'class' => 'btn btn-danger',
+      'data' => [
+        'confirm' => 'Apakah Anda yakin ingin menghapusnya?',
+        'method' => 'post',
+      ],
     ]) ?>
+  </p>
+
+  <?= DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+            // 'id',
+      [
+        'attribute' => 'id_anggota',
+        'value' => $model->anggota->nama
+      ],
+      [
+        'attribute' => 'id_buku',
+        'value' => $model->buku->nama
+      ],
+
+      [
+        'attribute' => 'tanggal_pinjam',
+        'value' => $model->tanggal_pinjam
+      ],
+      [
+        'attribute' => 'tanggal_kembali',
+        'value' => $model->tanggal_kembali
+      ],
+    ],
+  ]); ?>
+
 
 </div>
