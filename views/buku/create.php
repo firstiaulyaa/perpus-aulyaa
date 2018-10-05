e8c478ef0c9b3438589364a99089692a614778fc
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:03:29 +0700

    Tampilan create pada Buku

--- views/buku/create.php
+++ views/buku/create.php
@@ -6,16 +6,17 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Buku */
 
-$this->title = 'Create Buku';
-$this->params['breadcrumbs'][] = ['label' => 'Bukus', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Tambah Data Buku';
+$this->params['breadcrumbs'][] = ['label' => 'Buku', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title; // memanggil judul dari halaman bukuu
 ?>
-<div class="buku-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="buku-create">
+<div class="box box-primary">
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
 </div>
