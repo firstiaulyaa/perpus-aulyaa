3e99fcf28e0697b3b646b1953f949a1ebc31ba6c
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:32:49 +0700

    Tampilan create pada Penulis

--- views/penulis/create.php
+++ views/penulis/create.php
@@ -6,16 +6,19 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Penulis */
 
-$this->title = 'Create Penulis';
+$this->title = 'Tambah Data Penulis';
 $this->params['breadcrumbs'][] = ['label' => 'Penulis', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="penulis-create">
+<div class="box box-primary">
+<div class="box-body">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
+</div>
 </div>
