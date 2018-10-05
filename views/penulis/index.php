726d5f77feccbe4d31c7d7001f7552ec8b94c813
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:33:20 +0700

    Tampilan index pada Penulis

--- views/penulis/index.php
+++ views/penulis/index.php
@@ -4,19 +4,24 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\PenulisSearch */
+/* @var $searchModel app\models\penulisSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Penulis';
+$this->title = 'Data Penulis';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="penulis-index">
+<div class="box box-primary">
+<div class="box-body">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+     <p>
+        <?= Html::a('Tambah Data Penulis', ['create'], ['class' => 'btn btn-success']) ?>
 
-    <p>
-        <?= Html::a('Create Penulis', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Export ke Word', ['export-word'], ['class' => 'btn btn-info btn-flat']); ?>
+
+        <?= Html::a('Export ke Excel', ['export-excel'], ['class' => 'btn btn-success btn-flat']); ?>
+
+        <?= Html::a('Export ke PDF', ['site/export-pdf'], ['class' => 'btn btn-danger btn-flat']); ?>
     </p>
 
     <?= GridView::widget([
@@ -25,7 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            // 'id',
             'nama',
             'alamat:ntext',
             'telepon',
@@ -34,4 +39,6 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
+</div>
+</div>
 </div>
