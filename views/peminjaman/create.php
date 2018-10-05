73effe1fc57bdea4b37f1355b37ae1755d67f0f0
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:21:02 +0700

    Tampilan create pada Peminjaman

--- views/peminjaman/create.php
+++ views/peminjaman/create.php
@@ -6,16 +6,19 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Peminjaman */
 
-$this->title = 'Create Peminjaman';
-$this->params['breadcrumbs'][] = ['label' => 'Peminjamen', 'url' => ['index']];
+$this->title = 'Tambah Data Peminjaman';
+$this->params['breadcrumbs'][] = ['label' => 'Peminjaman', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="peminjaman-create">
+<div class="box box-primary">
+<div class="box-body">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
+</div>
 </div>
