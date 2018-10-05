dfa687805c28fc11eba5d8c611a5f87399348742
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:30:04 +0700

    Tampilan update pada Penerbit

--- views/penerbit/update.php
+++ views/penerbit/update.php
@@ -5,17 +5,19 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Penerbit */
 
-$this->title = 'Update Penerbit: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Penerbits', 'url' => ['index']];
+$this->title = 'Ubah Data Penerbit : ' . $model->nama;
+$this->params['breadcrumbs'][] = ['label' => 'Penerbit', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Ubah';
 ?>
 <div class="penerbit-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="box box-primary">
+<div class="box-body">
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
+</div>
 </div>
