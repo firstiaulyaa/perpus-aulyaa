c7cc8534fd81c3c7010b77c8d3a9e556f81d32c9
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:05:35 +0700

    Tampilan update pada Buku

--- views/buku/update.php
+++ views/buku/update.php
@@ -5,17 +5,17 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Buku */
 
-$this->title = 'Update Buku: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Bukus', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Ubah Data Buku : ' . $model->nama;
+$this->params['breadcrumbs'][] = ['label' => 'Buku', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->nama, 'url' => ['view', 'id' => $model->nama]];
+$this->params['breadcrumbs'][] = 'Ubah';
 ?>
 <div class="buku-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="box box-primary">
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
 </div>
