55383c22fe17b12479908c10f41ba2590123d564
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:19:20 +0700

    Tampilan view pada Kategori

--- views/kategori/view.php
+++ views/kategori/view.php
@@ -6,25 +6,32 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Kategori */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Kategoris', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = $model->nama; // memanggil nama pada data kategori
+$this->params['breadcrumbs'][] = ['label' => 'Kategori Buku', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title; // memanggil judul di halaman kategori
 ?>
+
+<!-- menampilkan view kategori -->
 <div class="kategori-view">
+<div class="box box-primary">
+<div class="box-body">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
+<!-- button ubah dan hapus satu data kategori -->
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Ubah', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Hapus', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Apakah Anda yakin ingin menghapusnya?',
                 'method' => 'post',
             ],
         ]) ?>
     </p>
+<!-- akhir button ubah dan hapus satu data kategori -->
 
+
+<!-- menampilkan satu data kategori -->
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
@@ -32,5 +39,37 @@ $this->params['breadcrumbs'][] = $this->title;
             'nama',
         ],
     ]) ?>
-
 </div>
+</div>
+</div>
+<!-- akhir menampilkan satu data kategori -->
+
+
+<!-- menampilkan daftar buku berdasarkan kategori -->
+<?php 
+
+?>
+<div>&nbsp;</div>
+<h3>Daftar Buku</h3>
+<div class="box box-primary">
+<div class="box-body">
+<table class="table">
+    <tr>
+        <th>No</th>
+        <th>Nama Buku</th>
+        <th>&nbsp;</th>
+    </tr>
+    <?php $no=1; foreach ($model->findAllBuku() as $buku): ?>
+    <tr>
+        <td><?= $no; ?></td>
+        <td><?= Html::a($buku->nama, ['buku/view', 'id' => $buku->id]); ?></td>
+        <td>
+            <?= Html::a("Ubah", ["buku/update","id"=>$buku->id], ['class' => 'btn btn-primary']); ?> &nbsp;
+            <?= Html::a("Hapus", ["buku/delete","id"=>$buku->id], ['class' => 'btn btn-danger'], ['data-method' => 'post', 'data-confirm' => 'Hapus data ?']); ?> &nbsp;
+        </td>
+    </tr>
+    <?php $no++; endforeach ?>
+</table>
+</div>
+</div>
+<!-- akhir menampilkan daftar buku berdasarkan kategori -->
