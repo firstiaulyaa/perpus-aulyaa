dfe14c9232aa98c93fa3f9f0ea4555ab2bd73489
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:45:43 +0700

    left pada themes layout

--- themes/adminlte/layouts/left.php
+++ themes/adminlte/layouts/left.php
@@ -1,3 +1,6 @@
+<?php
+use app\models\User;
+?>
 <aside class="main-sidebar">
 
     <section class="sidebar">
@@ -5,10 +8,12 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <!-- <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/> -->
+                <img src="<?= Yii::getAlias('@web').'/images/a.jpg'; ?>" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <!-- untuk menentukan siapa yg login -->
+                <p><?= Yii::$app->user->identity->username ?></p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
@@ -25,44 +30,104 @@
             </div>
         </form>
         <!-- /.search form -->
+        <!-- mengecek login -->
+        <?php if (User::isAdmin()) { ?>
+            <?= dmstr\Widgets\Menu::widget (
+            ) ?>
+        <?php } elseif (User::isAnggota()) { ?>
+            
+        <?= dmstr\Widgets\Menu::widget(
+        ) ?> 
+        <?php } ?>
 
+        <!-- Navigasi Admin -->
+        <?php if (Yii::$app->user->identity->id_user_role == 1): ?>
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
+                    ['label' => 'Dashboard', 'icon' => 'home', 'url' => ['site/dashboard']],
+                    ['label' => 'Data', 'options' => ['class' => 'header']],
+                    ['label' => 'Data Peminjaman', 'icon' => 'calendar-o', 'url' => ['peminjaman/index']],
+                    ['label' => 'Data Buku', 'icon' => 'book', 'url' => ['buku/index']],
+                    ['label' => 'Data Penerbit', 'icon' => 'building', 'url' => ['penerbit/index']],
+                    ['label' => 'Data Penulis', 'icon' => 'user', 'url' => ['penulis/index']],
+                    ['label' => 'Kategori Buku', 'icon' => 'server', 'url' => ['kategori/index']],
+                    ['label' => 'User', 'options' => ['class' => 'header']],
+                    ['label' => 'Data Anggota', 'icon' => 'users', 'url' => ['anggota/index']],
+                    ['label' => 'Data Petugas', 'icon' => 'user', 'url' => ['petugas/index']],
+                    ['label' => 'Data User', 'icon' => 'child', 'url' => ['user/index']],
+                    ['label' => 'Option', 'options' => ['class' => 'header']],
+                    ['label' => 'Logout', 'icon' => 'sign-out', 'url' => ['#']], 
                 ],
             ]
         ) ?>
+    <?php endif ?>
+    <!-- mengecek login -->
+        <?php if (User::isAnggota()) { ?>
+            <?= dmstr\Widgets\Menu::widget (
+            ) ?>
+        <?php } elseif (User::isPetugas()) { ?>
+            
+        <?= dmstr\Widgets\Menu::widget(
+        ) ?> 
+        <?php } ?>
+     <!-- Navigasi Anggota -->
+    <?php if (Yii::$app->user->identity->id_user_role == 2): ?>
+        <?= dmstr\widgets\Menu::widget(
+            [
+                    'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+                    'items' => [
+                        //['label' => 'Rumah', 'icon' => 'home', 'url' => ['site/index'],],
+                        ['label' => 'Home', 'icon' => 'home', 'url' => ['site/dashboard'],],
+                        ['label' => 'myLibrary', 'options' => ['class' => 'header']],
+                        // ['label' => 'Buku', 'icon' => 'book', 'url' => ['buku/index'],],
+                        ['label' => 'Peminjaman', 'icon' => 'calendar-o', 'url' => ['peminjaman/index'],],
+                        ['label' => 'Option', 'options' => ['class' => 'header']],
+                        ['label' => 'Logout', 'icon' => 'sign-out', 'url' => ['#']], 
+                    ],
+            ]
+        ) ?>
+    <?php endif ?>
+    <!-- mengecek login -->
+        <?php if (User::isPetugas()) { ?>
+            <?= dmstr\Widgets\Menu::widget (
+            ) ?>
+        <?php } elseif (User::isAdmin()) { ?>
+            
+        <?= dmstr\Widgets\Menu::widget(
+        ) ?> 
+        <?php } ?>
+    <!-- Navigasi Petugas -->
+    <?php if (Yii::$app->user->identity->id_user_role == 3): ?>
+        <?= dmstr\widgets\Menu::widget(
+            [
+                    'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+                    'items' => [
+                        //['label' => 'Rumah', 'icon' => 'home', 'url' => ['site/index'],],
+                        ['label' => 'Home', 'icon' => 'home', 'url' => ['site/dashboard'],],
+                        ['label' => 'Menu Buku', 'options' => ['class' => 'header']],
+                        [
+                            'label' => 'Buku',
+                            'icon' => 'book',
+                            'url' => '#',
+                            'items' => [
+                                ['label' => 'Buku', 'icon' => 'book', 'url' => ['buku/index'],],
+                                ['label' => 'Kategori', 'icon' => 'server', 'url' => ['kategori/index'],],
+                                ['label' => 'Penerbit', 'icon' => 'building', 'url' => ['penerbit/index'],],
+                                ['label' => 'Penulis', 'icon' => 'user', 'url' => ['penulis/index'],],
+                                ['label' => 'Peminjaman', 'icon' => 'calendar-o', 'url' => ['peminjaman/index'],],
+                            ],
+                        ],
+                        ['label' => 'Menu Pengguna', 'options' => ['class' => 'header']],
+                        ['label' => 'Anggota', 'icon' => 'user', 'url' => ['anggota/index'],],
+                        ['label' => 'Option', 'options' => ['class' => 'header']],
+                    ['label' => 'Logout', 'icon' => 'sign-out', 'url' => ['#']], 
+                        
+                    ],
+            ]
+        ) ?>
+    <?php endif ?>
 
     </section>
 
