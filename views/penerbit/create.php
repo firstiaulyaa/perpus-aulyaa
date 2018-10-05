37221acc4579ea5fb164c696448e626adfdba1d1
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:28:25 +0700

    Tampilan create pada Penerbit

--- views/penerbit/create.php
+++ views/penerbit/create.php
@@ -6,16 +6,19 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Penerbit */
 
-$this->title = 'Create Penerbit';
-$this->params['breadcrumbs'][] = ['label' => 'Penerbits', 'url' => ['index']];
+$this->title = 'Tambah Data Penerbit';
+$this->params['breadcrumbs'][] = ['label' => 'Penerbit', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="penerbit-create">
+<div class="box box-primary">
+<div class="box-body">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
+</div>
 </div>
