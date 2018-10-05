6cc7bd896a73cb507de41901470165bd380561a0
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 13:16:09 +0700

    Penerbit Controller

--- controllers/PenerbitController.php
+++ controllers/PenerbitController.php
@@ -3,11 +3,14 @@
 namespace app\controllers;
 
 use Yii;
-use app\models\Penerbit;
-use app\models\PenerbitSearch;
+use app\models\Penerbit; // untuk memanggil model Penerbit
+use app\models\penerbitSearch; // untuk memanggil model PenerbitSearch
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use PhpOffice\PhpWord\IOFactory; // untuk 
+use PhpOffice\PhpWord\PhpWord; // untuk memanggil ekstension PhpWord
+use PhpOffice\PhpWord\Shared\Converter; // untuk 
 
 /**
  * PenerbitController implements the CRUD actions for Penerbit model.
@@ -18,31 +21,45 @@ class PenerbitController extends Controller
      * {@inheritdoc}
      */
     public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
+        {
+            return [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
                 ],
-            ],
-        ];
-    }
+            ];
+        }
+
+
+
+    // ------------------------------------------------------------------ //
+    // action untuk menampilkan semua data pada Data Penerbit (index.php) //
+    // ---------------------------------------------------------------=-- //
 
     /**
      * Lists all Penerbit models.
      * @return mixed
      */
     public function actionIndex()
-    {
-        $searchModel = new PenerbitSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        {
+            $searchModel = new penerbitSearch();
+            $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
+            return $this->render('index', [
+                'searchModel' => $searchModel,
+                'dataProvider' => $dataProvider,
+            ]);
+        }
+
+    // ------------------------------------------------------------------ //
+
+
+
+    // --------------------------------------------------------------------------------- //
+    // action untuk menampilkan suatu data yang dipilih pada Data Penerbit pada view.php //
+    // --------------------------------------------------------------------------------- //
 
     /**
      * Displays a single Penerbit model.
@@ -51,11 +68,19 @@ class PenerbitController extends Controller
      * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
+        {
+            return $this->render('view', [
+                'model' => $this->findModel($id),
+            ]);
+        }
+
+    // --------------------------------------------------------------------------------- //
+
+
+
+    // ------------------------------------------------------------- //
+    // action untuk menambahkan penerbit ke database pada create.php //
+    // ------------------------------------------------------------- //
 
     /**
      * Creates a new Penerbit model.
@@ -63,17 +88,25 @@ class PenerbitController extends Controller
      * @return mixed
      */
     public function actionCreate()
-    {
-        $model = new Penerbit();
+        {
+            $model = new Penerbit();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            if ($model->load(Yii::$app->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+
+            return $this->render('create', [
+                'model' => $model,
+            ]);
         }
 
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
+    // ------------------------------------------------------------- //
+
+
+
+    // --------------------------------------------------------------- //
+    // action untuk melakukan update data yang dipilih pada update.php //
+    // --------------------------------------------------------------- //
 
     /**
      * Updates an existing Penerbit model.
@@ -83,17 +116,25 @@ class PenerbitController extends Controller
      * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
+        {
+            $model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            if ($model->load(Yii::$app->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+
+            return $this->render('update', [
+                'model' => $model,
+            ]);
         }
 
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
+    // --------------------------------------------------------------- //
+
+
+
+    // --------------------------------------------------------- //
+    // action untuk menghapus data yang dipilih di Data Penerbit //
+    // --------------------------------------------------------- //
 
     /**
      * Deletes an existing Penerbit model.
@@ -103,11 +144,19 @@ class PenerbitController extends Controller
      * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
+        {
+            $this->findModel($id)->delete();
 
-        return $this->redirect(['index']);
-    }
+            return $this->redirect(['index']);
+        }
+
+    // ------------------------------------------------------------- //
+
+
+
+    // -------------------------------------------------------- //
+    // action untuk memanggil model Data Penerbit di model lain //
+    // -------------------------------------------------------- //
 
     /**
      * Finds the Penerbit model based on its primary key value.
@@ -124,4 +173,6 @@ class PenerbitController extends Controller
 
         throw new NotFoundHttpException('The requested page does not exist.');
     }
+    
+    // ------------------------------------------------------------- //
 }
