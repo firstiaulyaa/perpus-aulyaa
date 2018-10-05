670cc5d2e1b817b081513a7c1dc6e422d24a1ed0
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:34:31 +0700

    Tampilan view pada Penulis

--- views/penulis/view.php
+++ views/penulis/view.php
@@ -6,20 +6,20 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Penulis */
 
-$this->title = $model->id;
+$this->title = $model->nama;
 $this->params['breadcrumbs'][] = ['label' => 'Penulis', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="penulis-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="box box-primary">
+<div class="box-body">
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Ubah', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Hapus', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Apakah Anda yakin ingin menghapusnya?',
                 'method' => 'post',
             ],
         ]) ?>
@@ -33,7 +33,37 @@ $this->params['breadcrumbs'][] = $this->title;
             'alamat:ntext',
             'telepon',
             'email:email',
+            [
+                'label' => 'Jumlah Buku',
+                'value' => $model->getJumlahBuku()
+            ]
         ],
     ]) ?>
+</div>
+</div>
+</div>
 
+<div>&nbsp;</div>
+<h3>Daftar Buku</h3>
+<div class="box box-primary">
+<div class="box-body">
+<table class="table">
+    <tr>
+        <th>No</th>
+        <th>Nama Buku</th>
+        <th>Penerbit</th>
+        <th>&nbsp;</th>
+    </tr>
+    <?php $no=1; foreach ($model->findAllBuku() as $buku): ?>
+    <tr>
+        <td><?= $no; ?></td>
+        <td><?= Html::a($buku->nama, ['buku/view', 'id' => $buku->id]); ?></td>
+        <td>
+            <?= Html::a("Sunting", ["buku/update","id"=>$buku->id], ['class' => 'btn btn-primary']); ?> &nbsp;
+            <?= Html::a("Hapus", ["buku/delete","id"=>$buku->id], ['class' => 'btn btn-danger'], ['data-method' => 'post', 'data-confirm' => 'Hapus data ?']); ?> &nbsp;
+        </td>
+    </tr>
+    <?php $no++; endforeach ?>
+</table>
+</div>
 </div>
