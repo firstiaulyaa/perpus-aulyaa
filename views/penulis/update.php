1241e43803f5be2dca552054f8ead3aee241a2e4
Firsti Aulya Khusnul Khotimah <firstiaulyaa@gmail.com>
2018-10-05 14:33:58 +0700

    Tampilan update pada Penulis

--- views/penulis/update.php
+++ views/penulis/update.php
@@ -5,17 +5,19 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Penulis */
 
-$this->title = 'Update Penulis: ' . $model->id;
+$this->title = 'Ubah Data Penulis : ' . $model->nama;
 $this->params['breadcrumbs'][] = ['label' => 'Penulis', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Ubah';
 ?>
 <div class="penulis-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="box box-primary">
+<div class="box-body">
 
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 
+</div>
+</div>
 </div>
